d15a9212b98db508a3c49da0504d7cd78f6da422
debysouza <deborasouza@id.uff.br>
2023-10-11 18:50:31 -0300

    feat: commit inicial.

---
+++ proj-ex/src/main/java/br/com/api/projex/ProjExApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.projex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjExApplication.class, args);
+	}
+
+}
---
+++ proj-ex/src/test/java/br/com/api/projex/ProjExApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.projex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
